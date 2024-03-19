4ed72aed66fc9005bedfe8b293bcd22aa5b20db7
夏雨 <MeiVinEight@outlook.com>
2024-03-19 11:44:10 +0800

    Manage memory block use AVL

---
+++ KERNEL/HEADER/memory/block.h
@@ -0,0 +1,23 @@
+#ifndef __KERNEL_MEMORY_BLOCK_H__
+#define __KERNEL_MEMORY_BLOCK_H__
+
+#include <types.h>
+
+typedef struct _MEMORY_BLOCK MEMORY_BLOCK;
+typedef struct _MEMORY_BLOCK
+{
+	MEMORY_BLOCK *P;
+	MEMORY_BLOCK *L;
+	MEMORY_BLOCK *R;
+	QWORD H;
+	QWORD A;
+	QWORD S;
+} MEMORY_BLOCK;
+
+MEMORY_BLOCK **NodeReference(MEMORY_BLOCK **, MEMORY_BLOCK *);
+QWORD TreeHeight(MEMORY_BLOCK *);
+void AdjustMemoryMap(MEMORY_BLOCK **, MEMORY_BLOCK *);
+void RemoveMemoryNode(MEMORY_BLOCK **, MEMORY_BLOCK *);
+void InsertMemoryNode(MEMORY_BLOCK **, MEMORY_BLOCK *);
+
+#endif
---
+++ KERNEL/SOURCE/memory/block.c
@@ -0,0 +1,196 @@
+#include <memory/block.h>
+#include <memory/heap.h>
+#include <declspec.h>
+
+/*
+* Get node reference:
+* Which is a pointer to the node's address.
+*/
+MEMORY_BLOCK **NodeReference(MEMORY_BLOCK **root, MEMORY_BLOCK *block)
+{
+	if (block->P)
+	{
+		if (block->P->L == block)
+		{
+			return &block->P->L;
+		}
+		else
+		{
+			return &block->P->R;
+		}
+	}
+	else
+	{
+		return root;
+	}
+}
+QWORD TreeHeight(MEMORY_BLOCK *tree)
+{
+	if (tree)
+	{
+		return tree->H;
+	}
+	return 0;
+}
+void AdjustMemoryMap(MEMORY_BLOCK **root, MEMORY_BLOCK *block)
+{
+	while (block)
+	{
+		MEMORY_BLOCK **reference = NodeReference(root, block);
+		MEMORY_BLOCK *P = block->P;
+		MEMORY_BLOCK *L = block->L;
+		MEMORY_BLOCK *R = block->R;
+		if (TreeHeight(block->L) > (TreeHeight(block->R) + 1))
+		{
+			// Right rotate
+			// Assume left child is not null
+			if (L->R)
+			{
+				L->R->P = block;
+			}
+			block->L = L->R;
+			block->P = L;
+			L->R = block;
+			L->P = P;
+			*reference = L;
+		}
+		else if (TreeHeight(block->R) > (TreeHeight(block->L) + 1))
+		{
+			// Left rotate
+			// Assume right child is not null
+			if (R->L)
+			{
+				R->L->P = block;
+			}
+			block->R = R->L;
+			block->P = R;
+			R->L = block;
+			R->P = P;
+			*reference = R;
+		}
+		QWORD LH = TreeHeight(block->L);
+		QWORD RH = TreeHeight(block->R);
+		block->H = LH > RH ? LH : RH;
+		block->H++;
+		block = block->P;
+	}
+}
+void RemoveMemoryNode(MEMORY_BLOCK **root, MEMORY_BLOCK *block)
+{
+	QWORD lc = (QWORD) block->L;
+	QWORD rc = (QWORD) block->R;
+	if (lc && rc)
+	{
+		// Left and right children are not null.
+		// Find the greatest node which is less than the block.
+		MEMORY_BLOCK *prev = block->L;
+		while (prev->R)
+		{
+			prev = prev->R;
+		}
+		// Copy data in prev to block, means block deleted and prev is invalid.
+		block->H = prev->H;
+		block->A = prev->A;
+		block->S = prev->S;
+		// Delete node prev.
+		// Because the node's right children is null,
+		// only needs to move the left children to parent's pointer.
+		*NodeReference(root, prev) = prev->L;
+		prev->L->P = prev->P;
+		// Make block point to the deleted node.
+		block = prev;
+	}
+	else
+	{
+		// Has up to one child
+		MEMORY_BLOCK *child = (MEMORY_BLOCK *) (lc | rc);
+		// Move the child to parent's pointer
+		*NodeReference(root, block) = child;
+		if (child)
+		{
+			child->P = block->P;
+		}
+	}
+	// Recursively adjust tree
+	AdjustMemoryMap(root, block->P);
+	// Free the memory of the node.
+	HeapFree(block);
+}
+void InsertMemoryNode(MEMORY_BLOCK **root, MEMORY_BLOCK *block)
+{
+	// Find smallest value which greater than the block
+	MEMORY_BLOCK tmp1 = {0, 0, 0, 0, ~(0ULL), 0};
+	MEMORY_BLOCK *next = &tmp1;
+	MEMORY_BLOCK *curr = *root;
+	while (curr)
+	{
+		if (curr->A >= block->A)
+		{
+			next = curr;
+			curr = curr->L;
+		}
+		else
+		{
+			curr = curr->R;
+		}
+	}
+
+	// Find bigest value which less than the block
+	MEMORY_BLOCK tmp2 = { 0, 0, 0, 0, 0, 0 };
+	MEMORY_BLOCK *prev = &tmp2;
+	curr = *root;
+	while (curr)
+	{
+		if (curr->A <= block->A)
+		{
+			prev = curr;
+			curr = curr->R;
+		}
+		else
+		{
+			curr = curr->L;
+		}
+	}
+
+	// Check merge
+	if (prev->A + prev->S >= block->A)
+	{
+		QWORD A = prev->A;
+		QWORD Z = block->A + block->S;
+		if (prev->A + prev->S > Z) Z = prev->A + prev->S;
+		block->A = A;
+		block->S = Z - A;
+		RemoveMemoryNode(root, prev);
+	}
+	if (block->A + block->S >= next->A)
+	{
+		QWORD A = block->A;
+		QWORD Z = next->A + next->S;
+		if (block->A + block->S > Z) Z = block->A + block->S;
+		block->A = A;
+		block->S = Z - A;
+		RemoveMemoryNode(root, next);
+	}
+
+	// Insert block into memory map
+	MEMORY_BLOCK *parent = 0;
+	MEMORY_BLOCK **reference = root;
+	while (*reference)
+	{
+		if (block->A > (*reference)->A)
+		{
+			// Insert into right subtree
+			parent = *reference;
+			reference = &(*reference)->R;
+		}
+		else
+		{
+			// Insert into left subtree
+			parent = *reference;
+			reference = &(*reference)->L;
+		}
+	}
+	block->P = parent;
+	*reference = block;
+	AdjustMemoryMap(root, block);
+}
