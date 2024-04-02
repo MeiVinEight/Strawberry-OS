6d9a01e945b54151b66a47b023d5c171e995c584
夏雨 <MeiVinEight@outlook.com>
2024-04-02 10:43:18 +0800

    Use macro instead of magic number

--- KERNEL/HEADER/memory/page.h
+++ KERNEL/HEADER/memory/page.h
@@ -3,6 +3,10 @@
 
 #include <types.h>
 
+#define PAGE4_4K 0
+#define PAGE4_2M 1
+#define PAGE4_1G 2
+
 void setup_paging();
 DWORD linear_mapping(QWORD, QWORD, BYTE, QWORD);
 DWORD identity_mapping(QWORD, BYTE);
